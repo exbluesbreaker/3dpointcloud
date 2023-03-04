d8a4945e9eb6a36591a01163721832c29dc9352b
exbluesbreaker <slayer-gurgen@yandex.ru>
2023-03-04 23:12:53 +0100

    Implement segmentation and lidar target to support transformation
    	- for now results with roll are very strange - needs
    	  investigation

--- lidarsim.py
+++ lidarsim.py
@@ -1,5 +1,25 @@
 import numpy as np
+from statistics import mean
+from pytransform3d import transformations as pt
 import math
+
+# get distance for given point from plane
+# which is perpendicular to X axis of lidar
+
+def get_rdist(hangle,vangle, dist_000):
+    r = dist_000/(math.cos(hangle)*math.cos(vangle))
+    return r
+
+# get coordinates for point hit by lidar
+# at hangle/vangle for plane
+# which is perpendicular to X axis of lidar
+def get_plane_xyz(hangle,vangle, dist_000):
+    r = get_rdist(hangle,vangle, dist_000)
+    x = dist_000
+    y = r*math.cos(vangle)*math.sin(hangle)
+    z = r*math.sin(vangle)
+    # return homogeneous coordinates
+    return (x,y,z,1)
 
 def ray_rect_intersection(u,p1,p2,p3,p4):
     # returns true if ray specified by vector u
@@ -23,21 +43,25 @@ def ray_rect_intersection(u,p1,p2,p3,p4):
             return False
 
 class LidarTarget:
-    def __init__(self,p1,p2,p3,p4):
-        self.p1 = p1
-        self.p2 = p2
-        self.p3 = p3
-        self.p4 = p4
-        self.v1 = p2 - p1
-        self.v2 = p3 - p1
-        self.v3 = p4 - p1
-        # manual cross product for speed
+    def __init__(self,points):
+        self.points = points
+        self.__calculate_vectors()
+
+    def __calculate_vectors(self):
+        # calculate vectors and values used for ray intersection
+        self.v1 = self.points[1,0:3] - self.points[0,0:3]
+        self.v2 = self.points[2,0:3] - self.points[0,0:3]
+        self.v3 = self.points[3,0:3] - self.points[0,0:3]
         self.n = np.cross(self.v1,self.v2)
-        self.dot_p1_v1 = np.dot(self.p1,self.v1)
-        self.dot_p2_v1 = np.dot(self.p2,self.v1)
-        self.dot_p1_v3 = np.dot(self.p1,self.v3)
-        self.dot_p4_v3 = np.dot(self.p4,self.v3)
-        self.dot_p1_n = (self.p1[0]*self.n[0]+self.p1[1]*self.n[1]+self.p1[2]*self.n[2])
+        self.dot_p1_v1 = np.dot(self.points[0,0:3],self.v1)
+        self.dot_p2_v1 = np.dot(self.points[1,0:3],self.v1)
+        self.dot_p1_v3 = np.dot(self.points[0,0:3],self.v3)
+        self.dot_p4_v3 = np.dot(self.points[3,0:3],self.v3)
+        self.dot_p1_n = np.dot(self.points[0,0:3],self.n)
+
+    def do_transformation(self, transformation):
+        self.points = pt.transform(transformation,self.points)
+        self.__calculate_vectors()
 
     def ray_rect_intersection(self,u):
         # returns true if ray specified by vector u
@@ -73,9 +97,12 @@ class LidarScene:
             self.sincos_cache["sin_va"].append(math.sin(self.vert_angles[v_idx]))
             self.sincos_cache["cos_va"].append(math.cos(self.vert_angles[v_idx]))
 
-    def add_rect(self,p1,p2,p3,p4):
-        new_rect = LidarTarget(p1,p2,p3,p4)
-        self.targets.append(new_rect)
+    def add_rect(self,target):
+        self.targets.append(target)
+
+    def transform_scene(self, transformation):
+        for t in self.targets:
+            t.do_transformation(transformation)
 
     def get_unit_vector(self,h_idx,v_idx):
         # return unit vector of lidar shot for given angles
@@ -99,3 +126,107 @@ class LidarScene:
                     if r.ray_rect_intersection(u):
                         scan[v_idx,h_idx] = 255
         return scan
+
+class Segmentation:
+    def __init__(self,dist_to_target,num_shots, num_layers,min_ha,max_ha,min_va,max_va):
+        self.dist_to_target = dist_to_target
+        self.num_shots = num_shots
+        self.num_layers = num_layers
+        self.hor_angles = np.linspace(min_ha,max_ha,num_shots)
+        self.vert_angles = np.linspace(min_va,max_va,num_layers)
+        pass
+
+    def do_segmentation(self,scan):
+        is_safe_dfs = lambda s,v,r,c: r>=0 and r<s.shape[0] and c>=0 and c<s.shape[1] and v[r][c] == False and s[r,c] != 0
+        shape = scan.shape
+        count = 0
+        visited = np.zeros((shape[0],shape[1]), np.bool)
+        self.segments = []
+        for row in range(shape[0]):
+            for col in range(shape[1]):
+                if visited[row][col] == False and scan[row,col] != 0:
+                    nodes_to_visit = [(row,col)];
+                    count += 1
+                    segment = []
+                    # DFS algorithm to detect segments
+                    while nodes_to_visit:
+                        dfs_row,dfs_col = nodes_to_visit.pop(0)
+                        if visited[dfs_row][dfs_col] == True:
+                            continue
+                        segment.append((dfs_row,dfs_col))
+                        visited[dfs_row][dfs_col] = True
+                        # use 4 connectivity for segments
+                        if is_safe_dfs(scan,visited,dfs_row-1,dfs_col):
+                            nodes_to_visit.append((dfs_row-1,dfs_col))
+                        if is_safe_dfs(scan,visited,dfs_row+1,dfs_col):
+                            nodes_to_visit.append((dfs_row+1,dfs_col))
+                        if is_safe_dfs(scan,visited,dfs_row,dfs_col-1):
+                            nodes_to_visit.append((dfs_row,dfs_col-1))
+                        if is_safe_dfs(scan,visited,dfs_row,dfs_col+1):
+                            nodes_to_visit.append((dfs_row,dfs_col+1))
+                    self.segments.append(segment)
+        # assign segments to target parts
+        self.extract_features()
+        self.assign_segments()
+
+    def get_segments(self):
+        return self.segments
+
+    def extract_features(self):
+        self.seg_feat = []
+        for idx in range(len(self.segments)):
+            seg = self.segments[idx]
+            v_idx = [pt[0] for pt in seg]
+            h_idx = [pt[1] for pt in seg]
+            self.seg_feat.append({"id":idx,
+                                  "v_min":min(v_idx),
+                                  "v_max":max(v_idx),
+                                  "v_mean":mean(v_idx),
+                                  "h_min":min(h_idx),
+                                  "h_max":max(h_idx),
+                                  "h_mean":mean(h_idx),
+                                  "size":len(seg)})
+
+    def assign_segments(self):
+        # detect top parts
+        # sort by horizontal length
+        self.seg_feat.sort(key=lambda a: a["h_max"]-a["h_min"],reverse=True)
+        tb1_top = self.segments[self.seg_feat[0]["id"]]
+        tb2_top = self.segments[self.seg_feat[1]["id"]]
+        tb1_bot_id = None
+        tb2_bot_id = None
+        # find bottom part for tb1
+        for i in range(2,len(self.seg_feat)):
+            # check segments in horizontal boundaries of tb1_top
+            if self.seg_feat[i]["h_mean"] >= self.seg_feat[0]["h_min"] and self.seg_feat[i]["h_mean"] <= self.seg_feat[0]["h_max"]:
+                # change currently assigned botom segment if we found a bigger one
+                if tb1_bot_id == None or self.seg_feat[i]["size"]>self.seg_feat[tb1_bot_id]["size"]:
+                    tb1_bot_id = i
+            # check segments in horizontal boundaries of tb2_top
+            if self.seg_feat[i]["h_mean"] >= self.seg_feat[1]["h_min"] and self.seg_feat[i]["h_mean"] <= self.seg_feat[1]["h_max"]:
+                # change currently assigned botom segment if we found a bigger one
+                if tb2_bot_id == None or self.seg_feat[i]["size"]>self.seg_feat[tb2_bot_id]["size"]:
+                    tb2_bot_id = i
+        if tb1_bot_id == None or tb2_bot_id == None:
+            print("Cannot find bottom segment")
+        tb1_bot = self.segments[self.seg_feat[tb1_bot_id]["id"]]
+        tb2_bot = self.segments[self.seg_feat[tb2_bot_id]["id"]]
+        self.tb1_top = tb1_top
+        self.tb2_top = tb2_top
+        self.tb1_bot = tb1_bot
+        self.tb2_bot = tb2_bot
+
+    def calculate_roll(self):
+        points_3d = []
+        for p in self.tb1_top:
+            point = get_plane_xyz(self.hor_angles[p[1]],self.vert_angles[p[0]],self.dist_to_target)
+            points_3d.append(point)
+        for p in self.tb2_top:
+            point = get_plane_xyz(self.hor_angles[p[1]],self.vert_angles[p[0]],self.dist_to_target)
+            points_3d.append(point)
+        x = [p[1] for p in points_3d]
+        y = [p[2] for p in points_3d]
+        roll, b = np.polyfit(x, y, 1)
+        return np.rad2deg(roll)
+
+        
--- run_lidarsim.py
+++ run_lidarsim.py
@@ -1,5 +1,9 @@
-import lidarsim as ls
+from lidarsim import LidarScene, LidarTarget, Segmentation
+from pytransform3d import transformations as pt
+from pytransform3d import rotations as pr
+from pytransform3d.transform_manager import TransformManager
 import numpy as np
+from math import sin,cos
 import cv2
 
 num_shots = 1008
@@ -9,22 +13,59 @@ ah_last = np.deg2rad(-63)
 av_first = np.deg2rad(9)
 av_last = np.deg2rad(-9)
 
-lscene = ls.LidarScene(num_shots,num_layers,ah_first,ah_last,av_first, av_last)
+lscene = LidarScene(num_shots,num_layers,ah_first,ah_last,av_first, av_last)
+
+rot_yaw = pt.transform_from(
+    pr.active_matrix_from_intrinsic_euler_xyz(np.array([0,0, np.deg2rad(25)])),
+    np.array([0, 0, 0]))
+
+tm = TransformManager()
+tm.add_transform("lidar000", "lidar_yaw", rot_yaw)
+transformation = tm.get_transform("lidar000", "lidar_yaw")
+
 # target 1
-dist_to_target = 750
-r1_p1 = np.asarray([dist_to_target, 450, 75])
-r1_p2 = np.asarray([dist_to_target, 250, 75])
-r1_p3 = np.asarray([dist_to_target, 250, -75])
-r1_p4 = np.asarray([dist_to_target, 450, -75])
-lscene.add_rect(r1_p1,r1_p2,r1_p3,r1_p4)
+# top part
+dist_to_target = 480
+pts = np.array([[dist_to_target, 183.5, 24.6, 1],
+                [dist_to_target, 153.5, 24.6, 1],
+                [dist_to_target, 153.5, 19.6, 1],
+                [dist_to_target, 183.5, 19.6, 1]])
+t1_top = LidarTarget(pts)
+lscene.add_rect(t1_top)
+# bottom part
+pts = np.array([[dist_to_target, 171, 14.6, 1],
+                [dist_to_target, 166, 14.6, 1],
+                [dist_to_target, 166, -16.6, 1],
+                [dist_to_target, 171, -16.6, 1]])
+t1_bot = LidarTarget(pts)
+lscene.add_rect(t1_bot)
 
-#target 2
-dist_to_target = 500
-r2_p1 = np.asarray([dist_to_target, -50,  25])
-r2_p2 = np.asarray([dist_to_target, -150, 25])
-r2_p3 = np.asarray([dist_to_target, -150, -25])
-r2_p4 = np.asarray([dist_to_target, -50, -25])
-lscene.add_rect(r2_p1,r2_p2,r2_p3,r2_p4)
+# target 2
+# top part
+dist_to_target = 480
+pts = np.array([[dist_to_target, -153.5, 24.6, 1],
+                [dist_to_target, -183.5, 24.6, 1],
+                [dist_to_target, -183.5, 19.6, 1],
+                [dist_to_target, -153.5, 19.6, 1]])
+t2_top   = LidarTarget(pts)
+lscene.add_rect(t2_top)
+# bottom part
+pts = np.array([[dist_to_target, -166, 14.6, 1],
+                [dist_to_target, -171, 14.6, 1],
+                [dist_to_target, -171, -16.6, 1],
+                [dist_to_target, -166, -16.6, 1]])
+t2_bot = LidarTarget(pts)
+lscene.add_rect(t2_bot)
 
+# rotate scene
+lscene.transform_scene(transformation)
+
+# do segmentation and calculater roll misalignment 
 scan = lscene.get_scan()
+segm = Segmentation(dist_to_target,num_shots,num_layers,ah_first,ah_last,av_first, av_last)
+segm.do_segmentation(scan)
+segments = segm.get_segments()
+roll = segm.calculate_roll()
+print(roll)
 cv2.imshow("Lidar scan",scan)
+cv2.waitKey(1)
